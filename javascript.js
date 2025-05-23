31f49a12d30b181f4a57866ae3a3feed7cd5e2e9
elrubicante <rubentnwork@gmail.com>
2025-05-22 22:26:22 -0400

    Add playRound function for RPS

--- javascript.js
+++ javascript.js
@@ -35,8 +35,47 @@ else { return ("put a correct input");}
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {}
- 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+
+
+if (humanSelection === "paper" && computerSelection === "rock")
+{humanScore++;
+console.log("You win! Paper beats rock. You saved humanity for now...");}
+
+else if (humanSelection === "paper" && computerSelection === "scissors")
+{computerScore++;
+console.log("You lose! Scissors beats paper. Humanity is in danger!");}
+
+else if (humanSelection === "paper" && computerSelection === "paper")
+{console.log("Is a tie, humanity and computer can be similar, right?");}
+
+else if (humanSelection === "rock" && computerSelection === "rock") 
+{console.log("Is a tie, humanity and computer can be similar, right?");}
+
+else if (humanSelection === "rock" && computerSelection === "scissors")
+{humanScore++;
+console.log("You win! Rock beats scissors. You saved humanity for now...");}
+
+else if (humanSelection === "rock" && computerSelection === "paper")
+{computerScore++;
+console.log("You lose! Paper beats rock. Humanity is in danger!");}
+
+else if (humanSelection === "scissors" && computerSelection === "paper")
+{humanScore++;
+console.log("You win! Scissors beats paper. You saved humanity for now...");}
+
+else if (humanSelection === "scissors" && computerSelection === "rock")
+{computerScore++;
+console.log("You lose! Rock beats scissors. Humanity is in danger!");}
+
+else { console.log("Maybe this is not the game for you");}
+
+}
+
+console.log(playRound(humanSelection, computerSelection));
+ 
+
 
